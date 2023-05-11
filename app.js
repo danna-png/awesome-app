287eb86b24428489e42b59eff1f1006e7717a7d9
danna-png <124694694+danna-png@users.noreply.github.com>
2023-05-11 14:50:40 +0000

    req y res

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ import express from 'express';
 //cear una instancia de epress
 const app = express(); // (req. res) => {Codigo}
 
+//registrar nuestro primer middleware
+app.use((req, res, next ) => {
+    console.log("Ejecutando el Middlware 1");
+
+//invocando al siguiente middlware
+next();
+});
+
+
+app.use((req, res, next) => {
+console.log(`${req.method} - ${req.url}`);
+next();
+});
+
+app.use((req, res) => {
+    console.log("Respondiendo al cliente");
+    res.send(`
+        <h1>Welcome to express</h1>
+        <p>This is my awesome app</p>
+        `);
+    });
+
 //creando el servidor
 const server = http.createServer(app);
 
