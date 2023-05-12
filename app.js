9678cc04ce737043b6b04437e290af1df17bc75c
danna-png <124694694+danna-png@users.noreply.github.com>
2023-05-12 15:44:18 +0000

    sin express

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //importar el modulo http
-import http from 'http';
+//import http from 'http';
 
 // importando expressjs
 import express from 'express';
@@ -24,20 +24,20 @@ next();
 app.use((req, res) => {
     console.log("Respondiendo al cliente");
     res.send(`
-        <h1>Welcome to express</h1>
+        <h1>Bienvenido a express</h1>
         <p>This is my awesome app</p>
         `);
     });
 
 //creando el servidor
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 //Definir puertos
 const port = 3000;
 const ip = "0.0.0.0";
 
 //arrancar el server
-server.listen(port, ip, ()=> { 
+app.listen(port, ip, ()=> { 
     console.log(" Sirviendo en http://localhost:3000");
-    console.log(`Sirviendo en http://${process.env.IP}:${process.env.PORT}`);
+    console.log(`Escuchando en http://${process.env.IP}:${process.env.PORT}`);
 });
